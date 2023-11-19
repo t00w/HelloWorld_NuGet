862c9ded9b7192aedf535890618fe22389bba616
negue-io <47615006+negue-io@users.noreply.github.com>
2023-11-19 09:43:09 +0100

    init

---
+++ src/HelloWorld_orHeWllrodl/HelloWorld.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloWorld_orHeWllrodl
+{
+    public static class HelloWorld
+    {
+        static readonly string HelloWorldStr = "Hello World";
+        public static string Hello() => HelloWorldStr;
+
+        public static string Shuffle()
+        {
+            char[] array = HelloWorldStr.ToCharArray();
+            Random rng = new Random();
+            int n = array.Length;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                var value = array[k];
+                array[k] = array[n];
+                array[n] = value;
+            }
+            return new string(array);
+        }
+    }
+}
